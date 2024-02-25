4fe3804e08137e5b02a76682b72f92474012041c
seongolee <seongo0521@gmail.com>
2024-02-25 19:01:05 +0900

    git init

---
+++ src/test/java/com/backtothefuture/BacktothefutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backtothefuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacktothefutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
