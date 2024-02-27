62bf4109544c1d918be06458f4ce9df86738c7bf
seongolee <57441776+seongolee@users.noreply.github.com>
2024-02-28 01:02:55 +0900

    feat - Exception Handler, GlobalErrorCode 추가 (#1)

---
+++ api/member/src/main/java/com/backtothefuture/member/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.backtothefuture.member.exception;
+
+import static com.backtothefuture.domain.enums.code.GlobalErrorCode.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.backtothefuture.domain.response.ErrorResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
+		log.error(">>>>> Internal Server Error : {}", ex);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(INTERNAL_SERVER_ERROR.getErrorResponse());
+	}
+}
