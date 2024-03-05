ae51acca8523afa599685b35d4b3cdffed409a6b
leesangmin <dkwkghkdlxld2@gmail.com>
2024-03-06 01:01:09 +0900

    feat: 기존 회원 OAuth 로그인 시 토큰 반환하는 로직 추가

--- api/member/src/main/java/com/backtothefuture/member/service/MemberService.java
+++ api/member/src/main/java/com/backtothefuture/member/service/MemberService.java
@@ -82,12 +82,35 @@ public class MemberService {
 		return memberRepository.save(member).getId();
 	}
 
-	/**
-	 * password, passwordConfirm 체크
-	 */
-	private void validatePassword(String password, String passwordConfirm) {
-		if(!password.equals(passwordConfirm)) {
-			throw new MemberException(PASSWORD_NOT_MATCHED);
-		}
+    /**
+     * password, passwordConfirm 체크
+     */
+    private void validatePassword(String password, String passwordConfirm) {
+        if (!password.equals(passwordConfirm)) {
+            throw new MemberException(PASSWORD_NOT_MATCHED);
+        }
+    }
+
+    /**
+     * OAuth 기존 회원 로그인
+     */
+    @Transactional
+    public LoginTokenDto OAuthLogin(Member member) {
+
+		UserDetailsImpl userDetail = (UserDetailsImpl) UserDetailsImpl.from(member);
+
+		// accessToken, refreshToken 생성
+		String accessToken = jwtProvider.createAccessToken(userDetail);
+		String refreshToken = jwtProvider.createRefreshToken();
+
+		LoginTokenDto loginTokenDto = LoginTokenDto.builder()
+			.accessToken(accessToken)
+			.refreshToken(refreshToken)
+			.build();
+
+		// redis 토큰 정보 저장 로직 추가해야 함
+
+		return loginTokenDto;
+
 	}
 }
