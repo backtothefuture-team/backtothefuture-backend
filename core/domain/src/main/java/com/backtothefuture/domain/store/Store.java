1903bf969dc25855b29227a713d785ef4c37392d
seongolee <seongo0521@gmail.com>
2024-03-08 20:29:50 +0900

    feat: Store Entity 생성

---
+++ core/domain/src/main/java/com/backtothefuture/domain/store/Store.java
@@ -0,0 +1,38 @@
+package com.backtothefuture.domain.store;
+
+import com.backtothefuture.domain.common.MutableBaseEntity;
+import com.backtothefuture.domain.member.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Store extends MutableBaseEntity {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "store_id")
+	private Long id;
+
+	private String name; 		// 가게 이름
+
+	@Lob
+	private String description; // 가게 설명
+
+	private String location;	// 가게 위치
+
+	private String contact;		// 연락처
+
+	private String image;		// 가게 이미지
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;		// 회원
+}
