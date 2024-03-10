de3b07551a724a5139a06102288af6c54bb9a162
seongolee <seongo0521@gmail.com>
2024-03-10 21:20:59 +0900

    fix: StoreRegisterDto null 값에 대해서 빈값으로 처리

--- api/store/src/main/java/com/backtothefuture/store/dto/request/StoreRegisterDto.java
+++ api/store/src/main/java/com/backtothefuture/store/dto/request/StoreRegisterDto.java
@@ -1,6 +1,7 @@
 package com.backtothefuture.store.dto.request;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.backtothefuture.domain.store.Store;
 import com.backtothefuture.store.annotation.NumericStringList;
@@ -21,19 +22,24 @@ public record StoreRegisterDto(
 	@Size(max = 50, message = "가게위치는 최대 50자 이하로 입력해주세요.")
 	String location,
 
-	@Size(max = 3, message = "연락처를 올바르게 입력해 주세요.")
 	@NumericStringList(message = "연락처는 숫자만 입력할 수 있습니다.")
 	List<String> contact,
 
 	String image
 ) {
 	public static Store toEntity(StoreRegisterDto storeRegisterDto){
+		String contact = Optional.ofNullable(storeRegisterDto.contact())
+			.map(num ->String.join("-", num))
+			.orElse("");
+
+		String image = Optional.ofNullable(storeRegisterDto.image()).orElse("");
+
 		return Store.builder()
 			.name(storeRegisterDto.name())
 			.description(storeRegisterDto.description())
 			.location(storeRegisterDto.location())
-			.contact(String.join("-", storeRegisterDto.contact()))
-			.image(storeRegisterDto.image())
+			.contact(contact)
+			.image(image)
 			.build();
 	}
 
