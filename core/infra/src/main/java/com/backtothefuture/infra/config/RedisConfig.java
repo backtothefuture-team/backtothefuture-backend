bb4b807836e54b32e2d938d9965322b856003ec7
Yuseon Lim <dogakday@gmail.com>
2024-03-12 01:12:43 +0900

    build: redis의 host를 redis의 servicename으로 지정 #34 (#38)
    * config: gitignore에 .env 추가
    
    * config: redis host redis service name으로 설정 #34

--- core/infra/src/main/java/com/backtothefuture/infra/config/RedisConfig.java
+++ core/infra/src/main/java/com/backtothefuture/infra/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.backtothefuture.infra.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -7,21 +9,26 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @Configuration
 public class RedisConfig {
-	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory();
-	}
 
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(connectionFactory);
-		template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-		return template;
-	}
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return template;
+    }
 }
