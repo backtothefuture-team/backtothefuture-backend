1f7d075783133dd831240a8c1be1ed5a8cc592b3
yuseon.lim <dogakday@gmail.com>
2024-03-18 22:23:49 +0900

    feat: 상품 조회 api 추가 #27

--- api/store/src/main/java/com/backtothefuture/store/controller/ProductController.java
+++ api/store/src/main/java/com/backtothefuture/store/controller/ProductController.java
@@ -8,27 +8,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 import static com.backtothefuture.domain.common.enums.GlobalSuccessCode.CREATE;
+import static com.backtothefuture.domain.common.enums.GlobalSuccessCode.SUCCESS;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/store")
 public class ProductController {
 
     private final ProductService productService;
 
     // 상품 등록 API
     // TODO: ROLE 을 STORE_OWNER, ADMIN 제한
-    @PostMapping("/{storeId}/products")
+    @PostMapping("/store/{storeId}/products")
     public ResponseEntity<BfResponse<?>> registerProduct(
             @PathVariable("storeId") Long storeId,
             @Valid @RequestBody ProductRegisterDto productRegisterDto
@@ -37,9 +37,27 @@ public class ProductController {
                 .body(new BfResponse<>(CREATE, Map.of("product_id", productService.registerProduct(storeId, productRegisterDto))));
     }
 
+    // 상품 단건 조회 API
+    @GetMapping("/store/{storeId}/products/{productId}")
+    public ResponseEntity<BfResponse<?>> getProduct(
+            @PathVariable Long storeId,
+            @PathVariable Long productId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new BfResponse<>(SUCCESS, Map.of("product", productService.getProduct(storeId, productId))));
+    }
+
+    // 모든 product 조회
+    // TODO: 정렬 기준, 페이지네이션 등 추가 ..
+    @GetMapping("/products")
+    public ResponseEntity<BfResponse<?>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new BfResponse<>(SUCCESS, Map.of("products", productService.getAllProducts())));
+    }
+
     // 상품 삭제 API
     // TODO: ROLE 을 STORE_OWNER, ADMIN 제한
-    @DeleteMapping("/{storeId}/products/{productId}")
+    @DeleteMapping("/store/{storeId}/products/{productId}")
     public ResponseEntity<BfResponse<?>> deleteProduct(
             @PathVariable("storeId") Long storeId,
             @PathVariable("productId") Long productId
