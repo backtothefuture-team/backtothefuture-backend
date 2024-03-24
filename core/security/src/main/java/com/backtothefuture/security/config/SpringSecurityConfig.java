576e5e21e6ea87d4443c56b23b157f6e9172e31b
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-03-25 01:32:13 +0900

    config: spring security endpoint 수정

--- core/security/src/main/java/com/backtothefuture/security/config/SpringSecurityConfig.java
+++ core/security/src/main/java/com/backtothefuture/security/config/SpringSecurityConfig.java
@@ -116,8 +116,7 @@ public class SpringSecurityConfig {
                 antMatcher(POST, "/member/register"),        // 회원가입
                 antMatcher(GET, "/store/{storeId}/products/{productId}"),        // 상품 단건 조회 API
                 antMatcher(GET, "/products"),                           // 상품 전체 조회 API
-                antMatcher(GET, "/certificate/message/**"), // 인증 번호 받기
-                antMatcher(POST, "/certificate/message") // 인증 번호 검증
+                antMatcher(POST, "/certificate/message/**") // 인증 번호 받기
         );
 
         return requestMatchers.toArray(RequestMatcher[]::new);
