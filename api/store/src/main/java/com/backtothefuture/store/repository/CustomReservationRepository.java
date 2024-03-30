e6a850fbec06f033c3d59a3da1df11b88bf6c9c2
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-03-30 09:38:07 +0900

    refactor: 파일 위치 변경

--- api/store/src/main/java/com/backtothefuture/store/repository/CustomReservationRepository.java
+++ api/store/src/main/java/com/backtothefuture/store/repository/CustomReservationRepository.java
@@ -1,23 +1,22 @@
-package com.backtothefuture.domain.reservation.repository;
+package com.backtothefuture.store.repository;
+
 
 import com.backtothefuture.domain.reservation.Reservation;
-
-import com.backtothefuture.domain.reservation.dto.ReservationResponseDto;
+import com.backtothefuture.store.dto.response.ReservationResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
-@Repository
 public interface CustomReservationRepository extends JpaRepository<Reservation, Long> {
 
-    @Query("select new com.backtothefuture.domain.reservation.dto.ReservationResponseDto(p.name,rp.quantity,p.price) "
+    @Query(value = "select new com.backtothefuture.domain.reservation.dto.ReservationResponseDto(p.name,rp.quantity,p.price) "
             + "from Member m "
             + "inner join Reservation r on r.member.id = m.id "
             + "left join ReservationProduct rp on r.id = rp.reservation.id "
             + "left join Product p on rp.product.id = p.id "
-            + "where m.id = :memberId and r.id = :reservationId")
+            + "where m.id = :memberId and r.id = :reservationId", nativeQuery = true)
     List<ReservationResponseDto> getReservation(@Param("memberId") Long memberId, @Param("reservationId") Long reservationId);
 }
