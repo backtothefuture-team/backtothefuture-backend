9b3937daf0b6cf527b8e4da2d1f85826d398d9d5
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-03-30 11:10:11 +0900

    feat: nativeQuery 비활성

--- api/store/src/main/java/com/backtothefuture/store/repository/CustomReservationRepository.java
+++ api/store/src/main/java/com/backtothefuture/store/repository/CustomReservationRepository.java
@@ -12,11 +12,11 @@ import java.util.List;
 
 public interface CustomReservationRepository extends JpaRepository<Reservation, Long> {
 
-    @Query(value = "select new com.backtothefuture.domain.reservation.dto.ReservationResponseDto(p.name,rp.quantity,p.price) "
+    @Query(value = "select new com.backtothefuture.store.dto.response.ReservationResponseDto(p.name,rp.quantity,p.price) "
             + "from Member m "
             + "inner join Reservation r on r.member.id = m.id "
             + "left join ReservationProduct rp on r.id = rp.reservation.id "
             + "left join Product p on rp.product.id = p.id "
-            + "where m.id = :memberId and r.id = :reservationId", nativeQuery = true)
+            + "where m.id = :memberId and r.id = :reservationId")
     List<ReservationResponseDto> getReservation(@Param("memberId") Long memberId, @Param("reservationId") Long reservationId);
 }
