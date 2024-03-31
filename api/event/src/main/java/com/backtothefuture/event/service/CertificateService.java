61a7b68b95e62c89a524feabb0aed44f569091b0
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-04-01 02:20:29 +0900

    style: 문자 인증 관련 주석 처리

--- api/event/src/main/java/com/backtothefuture/event/service/CertificateService.java
+++ api/event/src/main/java/com/backtothefuture/event/service/CertificateService.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 public class CertificateService {
 
-    private final CoolsmsService coolsmsService;
+//    private final CoolsmsService coolsmsService;
 
     private final RedisRepository redisRepository;
 
