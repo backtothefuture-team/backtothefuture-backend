a049e3d6c1af5c25330d562ffaef8fc018f6836d
jinny-l <jinny.l.dev@gmail.com>
2024-04-16 14:45:15 +0900

    feat: 정렬 옵션 도메인 구현 #148

--- core/domain/src/main/java/com/backtothefuture/domain/common/enums/StoreErrorCode.java
+++ core/domain/src/main/java/com/backtothefuture/domain/common/enums/StoreErrorCode.java
@@ -1,18 +1,23 @@
 package com.backtothefuture.domain.common.enums;
 
-import org.springframework.http.HttpStatus;
-
 import com.backtothefuture.domain.response.ErrorResponse;
-
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum StoreErrorCode implements BaseErrorCode {
+
+    // 400
     CHECK_MEMBER(400, "회원의 정보를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
     DUPLICATED_STORE_NAME(400, "이미 존재하는 가게입니다.", HttpStatus.BAD_REQUEST),
     DUPLICATED_HEART(400, "이미 찜한 상점입니다.", HttpStatus.BAD_REQUEST),
-    NOT_FOUND_STORE_ID(404, "가게가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     UNSUPPORTED_IMAGE_EXTENSION(400, "지원하지 않는 확장자 입니다. jpeg혹은 png 파일을 업로드 해주세요.", HttpStatus.BAD_REQUEST),
+    INVALID_SORTING_OPTION(400, "잘못된 정렬 옵션입니다.", HttpStatus.BAD_REQUEST),
+
+    // 404
+    NOT_FOUND_STORE_ID(404, "가게가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+
+    // 500
     IMAGE_UPLOAD_FAIL(500, "이미지 업로드에 실패했습니다. 관리자에게 문의해 주세요.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final int errorCode;
