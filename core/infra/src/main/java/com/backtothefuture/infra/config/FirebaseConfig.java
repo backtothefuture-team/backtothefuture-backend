85aa4f2b1e7cbb0eb9fcbe432a1b1a617b70433f
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-04-18 01:14:58 +0900

    feat: FCM SDK 초기화 추가 #26

---
+++ core/infra/src/main/java/com/backtothefuture/infra/config/FirebaseConfig.java
@@ -0,0 +1,40 @@
+package com.backtothefuture.infra.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${google.firebase.service.secret-key-path}")
+    private String GOOGLE_APPLICATION_CREDENTIALS;
+    @Value("${google.firebase.service.projectId}")
+    private String GOOGLE_PROJECT_ID;
+
+    /**
+     * Firebase SDK 초기화
+     */
+    @PostConstruct
+    private void init() {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream(GOOGLE_APPLICATION_CREDENTIALS);
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setProjectId(GOOGLE_PROJECT_ID)
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
