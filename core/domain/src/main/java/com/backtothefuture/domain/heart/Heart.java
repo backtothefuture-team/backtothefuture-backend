fbde3d0ed4b745bad9e03551e1907d069f7d4450
yuseon.lim <dogakday@gmail.com>
2024-04-21 20:41:51 +0900

    feat: heart 엔티티 추가 #132

---
+++ core/domain/src/main/java/com/backtothefuture/domain/heart/Heart.java
@@ -0,0 +1,37 @@
+package com.backtothefuture.domain.heart;
+
+import com.backtothefuture.domain.member.Member;
+import com.backtothefuture.domain.store.Store;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Heart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "heart_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
