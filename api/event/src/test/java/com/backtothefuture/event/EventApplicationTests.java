4cbef711c1e626fb50149ea11a5e74a4af114155
Lee sang min <dkwkghkdlxld2@gmail.com>
2024-04-29 22:04:52 +0900

    test: spring rest docs 삭제에 따른 관련 테스트 코드 주석처리 #143

--- api/event/src/test/java/com/backtothefuture/event/EventApplicationTests.java
+++ api/event/src/test/java/com/backtothefuture/event/EventApplicationTests.java
@@ -1,131 +1,131 @@
-package com.backtothefuture.event;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-import com.backtothefuture.domain.common.util.RandomNumUtil;
-import com.backtothefuture.domain.common.util.s3.S3AsyncUtil;
-import com.backtothefuture.domain.common.util.s3.S3Util;
-import com.backtothefuture.event.dto.request.MailCertificateRequestDto;
-import com.backtothefuture.event.service.CertificateService;
-import com.backtothefuture.infra.config.BfTestConfig;
-import com.backtothefuture.infra.config.S3Config;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-
-@ExtendWith(RestDocumentationExtension.class)
-@SpringBootTest
-@Slf4j
-class EventApplicationTests extends BfTestConfig {
-
-    private MockMvc mockMvc;
-
-    @MockBean
-    private CertificateService certificateService;
-
-    @MockBean
-    private RandomNumUtil randomNumUtil;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    // 임시 s3 관련 mockbean 설정..
-    @MockBean
-    private S3Util s3Util;
-
-    @MockBean
-    private S3Config s3Config;
-
-    @MockBean
-    private S3AsyncUtil s3AsyncUtil;
-
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
-
-//    @Test
-//    @DisplayName("인증 번호 발급 테스트")
-//    void getCertificateTest() throws Exception {
-//        //given
-//        String phoneNumber = "010-0000-0000";
-//        String randomNum = RandomNumUtil.createRandomNum(6);
-//        //when,then
-//        when(certificateService.getCertificateNumber(eq(phoneNumber))).thenReturn(anyString());
-//        this.mockMvc.perform(post("/certificate/message/{phoneNumber}", phoneNumber)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isCreated())
-//                .andDo(document("get-certificate-number",
-//                        resource(ResourceSnippetParameters.builder()
-//                                .description("인증 번호 발급 API입니다.")
-//                                .tags("certificate")
-//                                .summary("인증 번호 발급 API")
-//                                .pathParameters(
-//                                        parameterWithName("phoneNumber").type(SimpleType.NUMBER).description("휴대폰 번호")
-//                                )
-//                                .responseFields(
-//                                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
-//                                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
-//                                        fieldWithPath("data.certification_number").type(JsonFieldType.STRING).description("인증 번호")
-//                                )
-//                                .responseSchema(Schema.schema("[response] get-certificate-number")).build()
-//                        )));
+//package com.backtothefuture.event;
+//
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.when;
+//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+//import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+//import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+//
+//import com.backtothefuture.domain.common.util.RandomNumUtil;
+//import com.backtothefuture.domain.common.util.s3.S3AsyncUtil;
+//import com.backtothefuture.domain.common.util.s3.S3Util;
+//import com.backtothefuture.event.dto.request.MailCertificateRequestDto;
+//import com.backtothefuture.event.service.CertificateService;
+//import com.backtothefuture.infra.config.BfTestConfig;
+//import com.backtothefuture.infra.config.S3Config;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.restdocs.RestDocumentationContextProvider;
+//import org.springframework.restdocs.RestDocumentationExtension;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.context.WebApplicationContext;
+//
+//
+//@ExtendWith(RestDocumentationExtension.class)
+//@SpringBootTest
+//@Slf4j
+//class EventApplicationTests extends BfTestConfig {
+//
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private CertificateService certificateService;
+//
+//    @MockBean
+//    private RandomNumUtil randomNumUtil;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    // 임시 s3 관련 mockbean 설정..
+//    @MockBean
+//    private S3Util s3Util;
+//
+//    @MockBean
+//    private S3Config s3Config;
+//
+//    @MockBean
+//    private S3AsyncUtil s3AsyncUtil;
+//
+//    @BeforeEach
+//    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//                .apply(documentationConfiguration(restDocumentation))
+//                .build();
 //    }
 //
-//    @Test
-//    @DisplayName("인증 번호 검증 테스트")
-//    void verifyCertificateTest() throws Exception {
-//        //given
-//        String randomNum = RandomNumUtil.createRandomNum(6);
-//        Map<String, Object> verifyMap = new HashMap<>();
-//        verifyMap.put("phoneNumber", List.of("010", "1234", "5678"));
-//        verifyMap.put("certificationNumber", randomNum);
+////    @Test
+////    @DisplayName("인증 번호 발급 테스트")
+////    void getCertificateTest() throws Exception {
+////        //given
+////        String phoneNumber = "010-0000-0000";
+////        String randomNum = RandomNumUtil.createRandomNum(6);
+////        //when,then
+////        when(certificateService.getCertificateNumber(eq(phoneNumber))).thenReturn(anyString());
+////        this.mockMvc.perform(post("/certificate/message/{phoneNumber}", phoneNumber)
+////                        .contentType(MediaType.APPLICATION_JSON))
+////                .andExpect(status().isCreated())
+////                .andDo(document("get-certificate-number",
+////                        resource(ResourceSnippetParameters.builder()
+////                                .description("인증 번호 발급 API입니다.")
+////                                .tags("certificate")
+////                                .summary("인증 번호 발급 API")
+////                                .pathParameters(
+////                                        parameterWithName("phoneNumber").type(SimpleType.NUMBER).description("휴대폰 번호")
+////                                )
+////                                .responseFields(
+////                                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+////                                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+////                                        fieldWithPath("data.certification_number").type(JsonFieldType.STRING).description("인증 번호")
+////                                )
+////                                .responseSchema(Schema.schema("[response] get-certificate-number")).build()
+////                        )));
+////    }
+////
+////    @Test
+////    @DisplayName("인증 번호 검증 테스트")
+////    void verifyCertificateTest() throws Exception {
+////        //given
+////        String randomNum = RandomNumUtil.createRandomNum(6);
+////        Map<String, Object> verifyMap = new HashMap<>();
+////        verifyMap.put("phoneNumber", List.of("010", "1234", "5678"));
+////        verifyMap.put("certificationNumber", randomNum);
+////
+////        VerifyCertificateRequestDto request = new VerifyCertificateRequestDto(List.of("010", "1234", "5678"), randomNum);
+////
+////        //when,then
+////        this.mockMvc.perform(post("/certificate/message")
+////                        .contentType(MediaType.APPLICATION_JSON)
+////                        .content(objectMapper.writeValueAsString(verifyMap)))
+////                .andExpect(status().isOk())
+////                .andDo(document("verify-certificate-number",
+////                        resource(ResourceSnippetParameters.builder()
+////                                .description("인증 번호 검증 API입니다.")
+////                                .tags("certificate")
+////                                .summary("인증 번호 검증 API")
+////                                .requestFields(
+////                                        fieldWithPath("phoneNumber").type(JsonFieldType.ARRAY).description("전화번호"),
+////                                        fieldWithPath("certificationNumber").type(JsonFieldType.STRING).description("인증번호")
+////                                )
+////                                .requestSchema(Schema.schema("[request] verify-certificate-number"))
+////                                .responseFields(
+////                                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
+////                                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지")
+////                                )
+////                                .responseSchema(Schema.schema("[response] verify-certificate-number")).build()
+////                        )));
+////    }
 //
-//        VerifyCertificateRequestDto request = new VerifyCertificateRequestDto(List.of("010", "1234", "5678"), randomNum);
-//
-//        //when,then
-//        this.mockMvc.perform(post("/certificate/message")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(verifyMap)))
-//                .andExpect(status().isOk())
-//                .andDo(document("verify-certificate-number",
-//                        resource(ResourceSnippetParameters.builder()
-//                                .description("인증 번호 검증 API입니다.")
-//                                .tags("certificate")
-//                                .summary("인증 번호 검증 API")
-//                                .requestFields(
-//                                        fieldWithPath("phoneNumber").type(JsonFieldType.ARRAY).description("전화번호"),
-//                                        fieldWithPath("certificationNumber").type(JsonFieldType.STRING).description("인증번호")
-//                                )
-//                                .requestSchema(Schema.schema("[request] verify-certificate-number"))
-//                                .responseFields(
-//                                        fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 코드"),
-//                                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지")
-//                                )
-//                                .responseSchema(Schema.schema("[response] verify-certificate-number")).build()
-//                        )));
-//    }
-
-// TODO: 메일 전송/검증 API 테스트 작성
-}
+//// TODO: 메일 전송/검증 API 테스트 작성
+//}
--- api/store/src/test/java/com/backtothefuture/store/controller/ReservationControllerTest.java
+++ api/store/src/test/java/com/backtothefuture/store/controller/ReservationControllerTest.java
@@ -1,151 +1,151 @@
-package com.backtothefuture.store.controller;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-
-import com.backtothefuture.domain.common.util.s3.S3AsyncUtil;
-import com.backtothefuture.domain.common.util.s3.S3Util;
-import com.backtothefuture.infra.config.BfTestConfig;
-import com.backtothefuture.infra.config.S3Config;
-import com.backtothefuture.security.annotation.WithMockCustomUser;
-import com.backtothefuture.store.dto.request.ReservationRequestDto;
-import com.backtothefuture.store.dto.request.ReservationRequestItemDto;
-import com.backtothefuture.store.dto.response.ReservationResponseDto;
-import com.backtothefuture.store.service.ReservationService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.LocalDateTime;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import java.util.List;
-import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
-import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-
-
-@ExtendWith(RestDocumentationExtension.class)
-@SpringBootTest
-@ContextConfiguration
-class ReservationControllerTest extends BfTestConfig {
-
-
-    private MockMvc mockMvc;
-
-    @MockBean
-    private ReservationService mockReservationService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    // 임시 s3 관련 mockbean 설정..
-    @MockBean
-    private S3Util s3Util;
-
-    @MockBean
-    private S3Config s3Config;
-
-    @MockBean
-    private S3AsyncUtil s3AsyncUtil;
-
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext,
-               RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
-
-    @Test
-    @DisplayName("상품 예약하기")
-    @Disabled
-    @WithMockCustomUser
-    void makeReservation() throws Exception {
-
-        Long storeId = 1L;
-        Long product1Id = 1L;
-        Long product2Id = 2L;
-
-        ReservationRequestDto reservationRequestDto = ReservationRequestDto.builder()
-                .storeId(storeId)
-                .orderRequestItems(List.of(new ReservationRequestItemDto(product1Id, 1),
-                        new ReservationRequestItemDto(product2Id, 1)))
-                .reservationTime(LocalDateTime.of(2024,04,01,12, 00))
-                .build();
-
-        // TODO: 아래 코드가 테스트의 효과가 있는지 궁금합니다!
-        when(mockReservationService.makeReservation(anyLong(),
-                eq(reservationRequestDto))).thenReturn(
-                1L);
-
-        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/reservations")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(reservationRequestDto))
-                        .header("Authorization", "Bearer ${JWT Token}")
-                        .with(csrf()))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
-    }
-
-    @Test
-    @Disabled
-    @DisplayName("예약 조회하기")
-    @WithMockCustomUser
-    void getReservation() throws Exception {
-
-        Long reservationId = 1L;
-
-        List<ReservationResponseDto> reservationResponseDto = List.of(
-                new ReservationResponseDto("product1", 1, 1000),
-                new ReservationResponseDto("product2", 2, 2000)
-        );
-
-        when(mockReservationService.getReservation(any(), eq(reservationId))).thenReturn(
-                reservationResponseDto);
-
-        this.mockMvc.perform(
-                        RestDocumentationRequestBuilders.get("/reservations/{reservationId}", reservationId)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(reservationResponseDto))
-                                .header("Authorization", "Bearer ${JWT Token}"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-    }
-
-    @Test
-    @Disabled
-    @DisplayName("구매자 예약 취소하기")
-    @WithMockCustomUser
-    void cancelReservation() throws Exception {
-
-        Long reservationId = 1L;
-        doNothing().when(mockReservationService).cancelReservation(any(), eq(reservationId));
-
-        this.mockMvc.perform(
-                        RestDocumentationRequestBuilders.delete("/reservations/{reservationId}", reservationId)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .header("Authorization", "Bearer ${JWT Token}"))
-                .andExpect(MockMvcResultMatchers.status().isNoContent());
-    }
-}
+//package com.backtothefuture.store.controller;
+//
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.ArgumentMatchers.anyLong;
+//import static org.mockito.ArgumentMatchers.eq;
+//import static org.mockito.Mockito.doNothing;
+//import static org.mockito.Mockito.when;
+//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+//
+//import com.backtothefuture.domain.common.util.s3.S3AsyncUtil;
+//import com.backtothefuture.domain.common.util.s3.S3Util;
+//import com.backtothefuture.infra.config.BfTestConfig;
+//import com.backtothefuture.infra.config.S3Config;
+//import com.backtothefuture.security.annotation.WithMockCustomUser;
+//import com.backtothefuture.store.dto.request.ReservationRequestDto;
+//import com.backtothefuture.store.dto.request.ReservationRequestItemDto;
+//import com.backtothefuture.store.dto.response.ReservationResponseDto;
+//import com.backtothefuture.store.service.ReservationService;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import java.time.LocalDateTime;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.restdocs.RestDocumentationContextProvider;
+//import org.springframework.restdocs.RestDocumentationExtension;
+//import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.web.servlet.MockMvc;
+//import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.context.WebApplicationContext;
+//import java.util.List;
+//import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
+//import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+//import static org.mockito.ArgumentMatchers.*;
+//import static org.mockito.Mockito.*;
+//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+//import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+//
+//
+//@ExtendWith(RestDocumentationExtension.class)
+//@SpringBootTest
+//@ContextConfiguration
+//class ReservationControllerTest extends BfTestConfig {
+//
+//
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private ReservationService mockReservationService;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    // 임시 s3 관련 mockbean 설정..
+//    @MockBean
+//    private S3Util s3Util;
+//
+//    @MockBean
+//    private S3Config s3Config;
+//
+//    @MockBean
+//    private S3AsyncUtil s3AsyncUtil;
+//
+//    @BeforeEach
+//    void setUp(WebApplicationContext webApplicationContext,
+//               RestDocumentationContextProvider restDocumentation) {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//                .apply(documentationConfiguration(restDocumentation))
+//                .build();
+//    }
+//
+//    @Test
+//    @DisplayName("상품 예약하기")
+//    @Disabled
+//    @WithMockCustomUser
+//    void makeReservation() throws Exception {
+//
+//        Long storeId = 1L;
+//        Long product1Id = 1L;
+//        Long product2Id = 2L;
+//
+//        ReservationRequestDto reservationRequestDto = ReservationRequestDto.builder()
+//                .storeId(storeId)
+//                .orderRequestItems(List.of(new ReservationRequestItemDto(product1Id, 1),
+//                        new ReservationRequestItemDto(product2Id, 1)))
+//                .reservationTime(LocalDateTime.of(2024,04,01,12, 00))
+//                .build();
+//
+//        // TODO: 아래 코드가 테스트의 효과가 있는지 궁금합니다!
+//        when(mockReservationService.makeReservation(anyLong(),
+//                eq(reservationRequestDto))).thenReturn(
+//                1L);
+//
+//        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/reservations")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(objectMapper.writeValueAsString(reservationRequestDto))
+//                        .header("Authorization", "Bearer ${JWT Token}")
+//                        .with(csrf()))
+//                .andExpect(MockMvcResultMatchers.status().isCreated());
+//    }
+//
+//    @Test
+//    @Disabled
+//    @DisplayName("예약 조회하기")
+//    @WithMockCustomUser
+//    void getReservation() throws Exception {
+//
+//        Long reservationId = 1L;
+//
+//        List<ReservationResponseDto> reservationResponseDto = List.of(
+//                new ReservationResponseDto("product1", 1, 1000),
+//                new ReservationResponseDto("product2", 2, 2000)
+//        );
+//
+//        when(mockReservationService.getReservation(any(), eq(reservationId))).thenReturn(
+//                reservationResponseDto);
+//
+//        this.mockMvc.perform(
+//                        RestDocumentationRequestBuilders.get("/reservations/{reservationId}", reservationId)
+//                                .contentType(MediaType.APPLICATION_JSON)
+//                                .content(objectMapper.writeValueAsString(reservationResponseDto))
+//                                .header("Authorization", "Bearer ${JWT Token}"))
+//                .andExpect(MockMvcResultMatchers.status().isOk());
+//    }
+//
+//    @Test
+//    @Disabled
+//    @DisplayName("구매자 예약 취소하기")
+//    @WithMockCustomUser
+//    void cancelReservation() throws Exception {
+//
+//        Long reservationId = 1L;
+//        doNothing().when(mockReservationService).cancelReservation(any(), eq(reservationId));
+//
+//        this.mockMvc.perform(
+//                        RestDocumentationRequestBuilders.delete("/reservations/{reservationId}", reservationId)
+//                                .contentType(MediaType.APPLICATION_JSON)
+//                                .header("Authorization", "Bearer ${JWT Token}"))
+//                .andExpect(MockMvcResultMatchers.status().isNoContent());
+//    }
+//}
