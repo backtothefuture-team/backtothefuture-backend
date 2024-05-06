98c20888cbfb10f21835c8d2da33143ccddb5a8b
yuseon.lim <dogakday@gmail.com>
2024-05-06 22:42:54 +0900

    fix: repository 어노테이션 제거 #116

--- core/domain/src/main/java/com/backtothefuture/domain/term/repository/TermRepository.java
+++ core/domain/src/main/java/com/backtothefuture/domain/term/repository/TermRepository.java
@@ -2,8 +2,6 @@ package com.backtothefuture.domain.term.repository;
 
 import com.backtothefuture.domain.term.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TermRepository extends JpaRepository<Term, Long> {
 }
