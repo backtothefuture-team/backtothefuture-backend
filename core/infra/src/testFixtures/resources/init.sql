4b892287d0bc21b56ad6f5ec2175a53d6f7ecf30
yuseon.lim <dogakday@gmail.com>
2024-05-08 01:42:18 +0900

    refactor: 코드 정렬 #159

--- core/infra/src/testFixtures/resources/init.sql
+++ core/infra/src/testFixtures/resources/init.sql
@@ -7,44 +7,44 @@ USE test;
 
 CREATE TABLE member
 (
-    member_id    bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    auth_id      varchar(255),
-    email        varchar(255) UNIQUE,
-    name         varchar(255),
-    password     varchar(255),
-    phone_number varchar(255) UNIQUE,
-    status       varchar(255),
-    profile      varchar(255),
-    birth        date,
-    gender       char(1),
+    member_id          bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    auth_id            varchar(255),
+    email              varchar(255) UNIQUE,
+    name               varchar(255),
+    password           varchar(255),
+    phone_number       varchar(255) UNIQUE,
+    status             varchar(255),
+    profile            varchar(255),
+    birth              date,
+    gender             char(1),
     registration_token varchar(255),
-    provider     varchar(255),
-    roles        varchar(255),
-    updated_at   datetime(6),
-    updated_by   varchar(255),
-    created_at   datetime(6),
-    created_by   varchar(255)
+    provider           varchar(255),
+    roles              varchar(255),
+    updated_at         datetime(6),
+    updated_by         varchar(255),
+    created_at         datetime(6),
+    created_by         varchar(255)
 );
 
 
 CREATE TABLE store
 (
-    store_id            bigint          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    sorting_index       bigint,
-    name                varchar(255),
-    description         TEXT,
-    location            varchar(255),
-    contact             varchar(255),
-    image               varchar(255),
-    member_id           bigint NOT NULL,
-    updated_at          datetime(6),
-    updated_by          varchar(255),
-    created_at          datetime(6),
-    created_by          varchar(255),
-    average_rating      double,
-    total_rating_count  int,
-    start_time          time,
-    end_time            time,
+    store_id           bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sorting_index      bigint,
+    name               varchar(255),
+    description        TEXT,
+    location           varchar(255),
+    contact            varchar(255),
+    image              varchar(255),
+    member_id          bigint NOT NULL,
+    updated_at         datetime(6),
+    updated_by         varchar(255),
+    created_at         datetime(6),
+    created_by         varchar(255),
+    average_rating     double,
+    total_rating_count int,
+    start_time         time,
+    end_time           time,
     FOREIGN KEY (member_id) REFERENCES member (member_id),
     INDEX idx_sorting_index (sorting_index)
 );
@@ -184,16 +184,16 @@ CREATE TABLE reservation_status_history
 
 CREATE TABLE IF NOT EXISTS review
 (
-    review_id       BIGINT     NOT NULL    AUTO_INCREMENT PRIMARY KEY,
-    member_id       BIGINT     NOT NULL,
-    store_id        BIGINT     NOT NULL,
-    rating          DOUBLE     NOT NULL,
-    content         TEXT,
-    image_url       VARCHAR(255),
-    updated_at      datetime(6),
-    updated_by      varchar(255),
-    created_at      datetime(6),
-    created_by      varchar(255)
+    review_id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT NOT NULL,
+    store_id   BIGINT NOT NULL,
+    rating     DOUBLE NOT NULL,
+    content    TEXT,
+    image_url  VARCHAR(255),
+    updated_at datetime(6),
+    updated_by varchar(255),
+    created_at datetime(6),
+    created_by varchar(255)
 );
 
 INSERT INTO member (member_id, auth_id, email, name, password, phone_number, status, provider, roles, updated_at,
